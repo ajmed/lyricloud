fd3e9b7fc54e55bde4a7d15a0f9df1715d62c6c1
Alexander Medeiros <amedeiros@soundexchange.com>
2021-08-24 22:00:29 -0400

    render word cloud properly

--- src/app/d3cloud.service.ts
+++ src/app/d3cloud.service.ts
@@ -1,5 +1,5 @@
-import {LogService} from "./log/log.service";
-import {Injectable} from "@angular/core";
+import {LogService} from './log/log.service'
+import {Injectable} from '@angular/core'
 import * as d3 from 'd3v4'
 import wordCloud from 'd3-cloud'
 
@@ -13,29 +13,30 @@ export class D3cloudService {
   constructor(private log: LogService) {
   }
 
-
   draw(words) {
-    let fill = d3.scaleOrdinal(d3.schemeCategory10)
+    const fill = d3.scaleOrdinal(d3.schemeCategory10)
     d3.select('#word-cloud-canvas')
       .append('svg')
-      .attr('width', this.width)
-      .attr('height', this.height)
+      .style('width', this.width)
+      .style('height', this.height)
+      .style('overflow', 'visible')
       .append('g')
-      .attr('transform', 'translate(150,150)')
+      .style('transform', 'translate(1000px,-256px)')
+      .style('overflow', 'visible')
       .selectAll('text')
       .data(words)
       .enter()
       .append('text')
       .style('font-size', function(d) { return d.size + 'px' } )
       .style('font-family', 'Impact')
-      .style('fill', function(d, i) { return fill(i); })
+      .style('fill', function(d, i) { return fill(i) })
       .attr('text-anchor', 'middle')
       .attr('transform', function(d) { return `translate(${d.x}, ${d.y}) rotate(${d.rotate})` })
       .text(function(d) { return d.text })
   }
 
   mapWords(words) {
-    return words.map(word => { return {text: word, size: 10 + Math.random() * 90} })
+    return words.map(word => ({text: word, size: 10 + Math.random() * 90}))
   }
 
   start(canvas, words) {
@@ -43,12 +44,12 @@ export class D3cloudService {
       .size([this.width, this.height])
       .words(this.mapWords(words))
       .padding(5)
-      .rotate(function() { return ~~(Math.random() * 2) * 90; })
-      .font("Impact")
-      .fontSize(function(d) { return d.size; })
+      /*.rotate(function() { return ~~(Math.random() * 2) * 90 })*/
+      .font('Impact')
+      .fontSize(function(d) { return d.size })
       /*.on("word", (wordObject) => { this.onWordPlaced(wordObject) })*/
-      .on("end", this.draw)
-      .start();
+      .on('end', this.draw)
+      .start()
   }
 
   /*private onWordPlaced(wordObject) {
